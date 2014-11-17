8da773f51b3655eae742dbd8c9528614147b7bb1
Alexander Graefenstein <mail@ginie.eu>
2014-11-17 13:51:57 +0100

    Layout#initialize patch refactored

--- lib/jekyll/slim/extensions.rb
+++ lib/jekyll/slim/extensions.rb
@@ -1,34 +1,14 @@
 module Jekyll
   class Layout
-    # path is required
-    # Also related to https://github.com/mojombo/jekyll/issues/225
-    def path
-      @name
+
+    def initialize_with_transform(*args)
+      initialize_without_transform(*args)
+      self.content = transform
     end
 
-    # Allows layouts to be transformed by slim until this is fixed (1.4)
-    # https://github.com/mojombo/jekyll/issues/225
-    # https://github.com/mojombo/jekyll/blob/master/lib/jekyll/layout.rb
-    alias old_initialize initialize
-    def initialize(*args)
-      old_initialize(*args)
-      transform
-    end
+    alias_method :initialize_without_transform, :initialize
+    alias_method :initialize, :initialize_with_transform
 
-    def to_liquid
-      data.merge(name: name, content: content)
-    end
   end
 
-  module Convertible
-    class<< self
-      attr_accessor :slim_current_convertible
-    end
-
-    alias old_transform transform
-    def transform
-      Convertible.slim_current_convertible = self
-      old_transform
-    end
-  end
 end
