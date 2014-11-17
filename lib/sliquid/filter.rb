b1e8630dce26df47a753ee5e09738b14e4f515f2
Marcel Jackwerth <marceljackwerth@gmail.com>
2014-11-17 15:34:27 +0100

    Recursive compilation of liquid tags

--- lib/sliquid/filter.rb
+++ lib/sliquid/filter.rb
@@ -2,7 +2,7 @@ module Sliquid
   class Filter < Slim::Filter
     def on_liquid_tag(name, args, block)
       result = [:static, "{% #{name} #{args} %}"]
-      result = [:multi, result, block] unless empty_exp?(block)
+      result = [:multi, result, compile(block)] unless empty_exp?(block)
       result
     end
   end
