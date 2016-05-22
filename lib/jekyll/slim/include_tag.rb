12c6c3f8a980c45b5209d147b9b1132a1c686b53
mcnelson <michael@nelsonware.com>
2016-05-22 13:13:46 -0700

    Fix include tag extension

--- lib/jekyll/slim/include_tag.rb
+++ lib/jekyll/slim/include_tag.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module Slim
     class IncludeTag < Jekyll::Tags::IncludeTag
-      def source(file, context)
+      def read_file(file, context)
         content = super
         if file =~ /\.slim\Z/i
           Converter.convert(context.registers[:site].config, content)
